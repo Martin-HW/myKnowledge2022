e3bdd4d9d0b9253f2b8c40a101c13f709e75ab8c
martin_suarez <martin_suarez_mendez@outlook.com>
2022-12-28 17:11:34 -0600

    menu done and deployed

--- react-exercises.js
+++ react-exercises.js
@@ -4,3 +4,6 @@ let toursVercel_02 = "https://tours-martin-here.vercel.app/";
 let toursGithub_02 = "https://github.com/Martin-HW/toursMartinHere";
 let reviewsVercel_03 = "https://reviews-martin-here.vercel.app/";
 let reviewsGithub_03 = "https://github.com/Martin-HW/reviewsMartinHere";
+
+let menuVercel_05 = "https://menu-martin-here.vercel.app/";
+let menuGithub_05 = "https://github.com/Martin-HW/menuMartinHere";
