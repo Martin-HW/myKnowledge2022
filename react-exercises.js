9e58ad6c0c14198c66f788a0442b21800a21887a
martin_suarez <martin_suarez_mendez@outlook.com>
2022-12-28 16:43:52 -0600

    reviews donde and deployed

--- react-exercises.js
+++ react-exercises.js
@@ -2,3 +2,5 @@ let birthdayVercel_01 = "https://birthday-martin-here.vercel.app/";
 let birthdayGithub_01 = "https://github.com/Martin-HW/birthdayMartinHere";
 let toursVercel_02 = "https://tours-martin-here.vercel.app/";
 let toursGithub_02 = "https://github.com/Martin-HW/toursMartinHere";
+let reviewsVercel_03 = "https://reviews-martin-here.vercel.app/";
+let reviewsGithub_03 = "https://github.com/Martin-HW/reviewsMartinHere";
