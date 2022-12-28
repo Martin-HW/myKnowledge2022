64d90810bc979b274c5ae6653065a5139f293761
martin_suarez <martin_suarez_mendez@outlook.com>
2022-12-28 13:22:03 -0600

    tours done and deployed

--- react-exercises.js
+++ react-exercises.js
@@ -1,2 +1,4 @@
 let birthdayVercel_01 = "https://birthday-martin-here.vercel.app/";
 let birthdayGithub_01 = "https://github.com/Martin-HW/birthdayMartinHere";
+let toursVercel_02 = "https://tours-martin-here.vercel.app/";
+let toursGithub_02 = "https://github.com/Martin-HW/toursMartinHere";
